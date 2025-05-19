4bb8285518aab9266f33515fef11712e866f61b1
choco090210 <jeremi.orlowski@gmail.com>
2025-05-19 09:59:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,21 @@
 #include <iostream>
-#include <cmath>
 #include "geometry.h"
 
 using namespace std;
 
 int main()
 {
-int n = 0;
-cout << "Podaj wartosc n w zaleznosci od tego ktorego programu chcesz uzyc 1 - dystans, 2 - czy to trojkat, 3- pole trojkata, 4- pole pieciakata, 5 - czy punkt jest w trojkacie" << endl;
-cin >> n;
-if (n==1)
-{
- point A;
- point B;
- cout << "Wczytaj punkt A" << endl;
- cin >> A.x >> A.y;
- cout << "Wczytaj punkt B" << endl;
- cin >> B.x >> B.y;
- cout << "Odleglosc to: " << distance(A,B) << endl;
-}
-if (n==2)
-{
- point A;
- point B;
- point C;
- cout << "Wczytaj punkt A" << endl;
- cin >> A.x >> A.y;
- cout << "Wczytaj punkt B" << endl;
- cin >> B.x >> B.y;
- cout << "Wczytaj punkt C" << endl;
- cin >> C.x >> C.y;
- if(is_triangle(A,B,C) == true)
- cout << "To jest trojkat" << endl;
- else 
- cout << "To nie jest trojkat" << endl;
-}
-if (n==3)
-{
- point A;
- point B;
- point C;
- cout << "Wczytaj punkt A" << endl;
- cin >> A.x >> A.y;
- cout << "Wczytaj punkt B" << endl;
- cin >> B.x >> B.y;
- cout << "Wczytaj punkt C" << endl;
- cin >> C.x >> C.y;
- cout << area_triangle(A,B,C) << endl;
-}
-if (n==4)
-{
- point A;
- point B;
- point C;
- point D;
- point E;
- cout << "Wczytaj punkt A" << endl;
- cin >> A.x >> A.y;
- cout << "Wczytaj punkt B" << endl;
- cin >> B.x >> B.y;
- cout << "Wczytaj punkt C" << endl;
- cin >> C.x >> C.y;
- cout << "Wczytaj punkt D" << endl;
- cin >> D.x >> D.y;
- cout << "Wczytaj punkt E" << endl;
- cin >> E.x >> E.y; 
- cout << "Pole to :" << area_pentagon(A,B,C,D,E) << endl;
-}
-if (n==5)
-{
- point A;
- point B;
- point C;
- point D;
- cout << "Wczytaj punkt A" << endl;
- cin >> A.x >> A.y;
- cout << "Wczytaj punkt B" << endl;
- cin >> B.x >> B.y;
- cout << "Wczytaj punkt C" << endl;
- cin >> C.x >> C.y;
- cout << "Wczytaj punkt D" << endl;
- cin >> D.x >> D.y;
- if(is_inside_triangle(A,B,C,D) == true)
- cout << "Punkt jest w trojkacie" << endl;
- else
- cout << "Punkt nie jest w trojkacie" << endl;
-}
-if (n != 1 && n != 2 && n != 3 && n != 4 && n!= 5)
-{
-    cout << "Bruh" << endl;
-    exit(0);
-}
-return 0;
+    point A = {1, 2};
+    point B = {3, 4};
+	point C = {7, 6};
+	point D = {2, 5};
+	point P[5] = { {1, 2}, {3, 4}, {5, 4}, {7, -1}, {3, -2} };
+	cout << "Distance: : " << distance(A, B) << endl;
+	cout << "Is it a triangle? (yes - 1, no - 0): " << is_triangle(A, B, C) << endl;
+	cout << "Area of triangle: " << area_triangle(A, B, C) << endl;
+	cout << "Area of polygon: " << area_polygon(P, sizeof(P) / sizeof(P[0])) << endl;
+	cout << "Is this point inside? (yes - 1, no - 0): " << is_inside(A, B, C, D) << endl;
+
+    // pole wielokąta P5 = 23
+    return 0;
 }
